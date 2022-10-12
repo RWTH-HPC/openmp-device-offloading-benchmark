ed60fc5b09aff5be9de7ffdf7d26ee8aab55e166
Jannis Klinkenberg <j.klinkenberg@itc.rwth-aachen.de>
2022-10-12 12:27:47 +0200

    topo output to results directory

--- scripts/amd_get_topo_info.sh
+++ scripts/amd_get_topo_info.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-sbatch --partition=mi210 --output=topo_mi210.txt amd_get_topo_info.sbatch
-sbatch --partition=mi250 --output=topo_mi250.txt amd_get_topo_info.sbatch
+RES_DIR="$(pwd)/results"
+mkdir -p ${RES_DIR}
+
+sbatch --partition=mi210 --output=${RES_DIR}/topo_mi210.txt amd_get_topo_info.sbatch
+sbatch --partition=mi250 --output=${RES_DIR}/topo_mi250.txt amd_get_topo_info.sbatch
--- scripts/nvidia_get_topo_info.sh
+++ scripts/nvidia_get_topo_info.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-sbatch --partition=c16g --gres=gpu:pascal:2 --account=supp0001 --output=topo_c16g.txt nvidia_get_topo_info.sbatch
-sbatch --partition=c16g --gres=gpu:2 --account=supp0001 --output=topo_c18g.txt nvidia_get_topo_info.sbatch
+RES_DIR="$(pwd)/results"
+mkdir -p ${RES_DIR}
+
+sbatch --partition=c16g --gres=gpu:pascal:2 --account=supp0001 --output=${RES_DIR}/topo_c16g.txt nvidia_get_topo_info.sbatch
+sbatch --partition=c16g --gres=gpu:2 --account=supp0001 --output=${RES_DIR}/topo_c18g.txt nvidia_get_topo_info.sbatch
