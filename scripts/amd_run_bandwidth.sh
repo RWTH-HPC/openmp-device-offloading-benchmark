1ed143df4fbd104da0cc19d86f67e6ebf1b7b394
Jannis Klinkenberg <j.klinkenberg@itc.rwth-aachen.de>
2023-01-27 11:04:14 +0100

    for now only run async bw benchmark without measuring allocation/free

--- scripts/amd_run_bandwidth.sh
+++ scripts/amd_run_bandwidth.sh
@@ -11,17 +11,17 @@ export GPU_ARCH=mi210
 sbatch --partition=mi210 --export=GPU_ARCH,USE_HIP --output=${RES_DIR}/results_bw_mi210.txt amd_run_bandwidth.sbatch
 
 # HIP based (w/ alloc included)
-export USE_HIP=1
-export INCLUDE_ALLOC=1
-export GPU_ARCH=mi250
-sbatch --partition=mi250 --export=GPU_ARCH,USE_HIP,INCLUDE_ALLOC --output=${RES_DIR}/results_bw_mi250-hip.txt amd_run_bandwidth.sbatch
-export GPU_ARCH=mi210
-sbatch --partition=mi210 --export=GPU_ARCH,USE_HIP,INCLUDE_ALLOC --output=${RES_DIR}/results_bw_mi210-hip.txt amd_run_bandwidth.sbatch
+# export USE_HIP=1
+# export INCLUDE_ALLOC=1
+# export GPU_ARCH=mi250
+# sbatch --partition=mi250 --export=GPU_ARCH,USE_HIP,INCLUDE_ALLOC --output=${RES_DIR}/results_bw_mi250-hip_incl-alloc.txt amd_run_bandwidth.sbatch
+# export GPU_ARCH=mi210
+# sbatch --partition=mi210 --export=GPU_ARCH,USE_HIP,INCLUDE_ALLOC --output=${RES_DIR}/results_bw_mi210-hip_incl-alloc.txt amd_run_bandwidth.sbatch
 
 # HIP based (w/o alloc included)
 export USE_HIP=1
 export INCLUDE_ALLOC=0
 export GPU_ARCH=mi250
-sbatch --partition=mi250 --export=GPU_ARCH,USE_HIP,INCLUDE_ALLOC --output=${RES_DIR}/results_bw_mi250-hip-noalloc.txt amd_run_bandwidth.sbatch
+sbatch --partition=mi250 --export=GPU_ARCH,USE_HIP,INCLUDE_ALLOC --output=${RES_DIR}/results_bw_mi250-hip.txt amd_run_bandwidth.sbatch
 export GPU_ARCH=mi210
-sbatch --partition=mi210 --export=GPU_ARCH,USE_HIP,INCLUDE_ALLOC --output=${RES_DIR}/results_bw_mi210-hip-noalloc.txt amd_run_bandwidth.sbatch
+sbatch --partition=mi210 --export=GPU_ARCH,USE_HIP,INCLUDE_ALLOC --output=${RES_DIR}/results_bw_mi210-hip.txt amd_run_bandwidth.sbatch
