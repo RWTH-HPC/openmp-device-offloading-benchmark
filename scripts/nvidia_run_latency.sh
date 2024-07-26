4f57086fbd0b98a5124eedcdf75257f40d3577d9
Jannis Klinkenberg <j.klinkenberg@itc.rwth-aachen.de>
2024-07-26 10:18:05 +0200

    specify number of tasks

--- scripts/nvidia_run_latency.sh
+++ scripts/nvidia_run_latency.sh
@@ -10,15 +10,15 @@ export USE_CUDA=0
 
 # CLAIX 2016
 export GPU_ARCH=sm_60
-sbatch --partition=c16g --gres=gpu:pascal:2 --account=supp0001 --export=GPU_ARCH,USE_CUDA --output=${RES_DIR}/results_lat_c16g.txt nvidia_run_latency.sbatch
+sbatch --partition=c16g --ntasks-per-node=24 --gres=gpu:pascal:2 --account=supp0001 --export=GPU_ARCH,USE_CUDA --output=${RES_DIR}/results_lat_c16g.txt nvidia_run_latency.sbatch
 
 # CLAIX 2018
 export GPU_ARCH=sm_70
-sbatch --partition=c18g --gres=gpu:2 --account=supp0001 --export=GPU_ARCH,USE_CUDA --output=${RES_DIR}/results_lat_c18g.txt nvidia_run_latency.sbatch
+sbatch --partition=c18g --ntasks-per-node=48 --gres=gpu:2 --account=supp0001 --export=GPU_ARCH,USE_CUDA --output=${RES_DIR}/results_lat_c18g.txt nvidia_run_latency.sbatch
 
 # CLAIX 2023
 export GPU_ARCH=sm_90
-sbatch --partition=c23g --gres=gpu:2 --account=supp0001 --export=GPU_ARCH,USE_CUDA --output=${RES_DIR}/results_lat_c23g.txt nvidia_run_latency.sbatch
+sbatch --partition=c23g --ntasks-per-node=96 --gres=gpu:4 --account=supp0001 --export=GPU_ARCH,USE_CUDA --output=${RES_DIR}/results_lat_c23g.txt nvidia_run_latency.sbatch
 
 ####################################################
 ### CUDA based
