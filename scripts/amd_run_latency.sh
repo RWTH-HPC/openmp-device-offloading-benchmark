55add8829d7111522328abec53eb2d23d0dcd3a0
Jannis Klinkenberg <j.klinkenberg@itc.rwth-aachen.de>
2024-08-01 15:50:16 +0200

    hopefully fixed issue with MPI+offloading on AMD

--- scripts/amd_run_latency.sh
+++ scripts/amd_run_latency.sh
@@ -2,7 +2,7 @@
 
 RES_DIR="$(pwd)/results"
 mkdir -p ${RES_DIR}
-COMMON_EXPORTS="GPU_ARCH,USE_CUDA,CPU_BIND"
+COMMON_EXPORTS="GPU_ARCH,USE_CUDA,MPICMD"
 
 ####################################################
 ### OpenMP target based
@@ -10,11 +10,11 @@ COMMON_EXPORTS="GPU_ARCH,USE_CUDA,CPU_BIND"
 export USE_HIP=0
 
 export GPU_ARCH=mi250
-export CPU_BIND="--cpu-bind=map_ldom:0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7"
+export MPICMD="mpirun -np 16 --map-by ppr:2:numa --oversubscribe"
 sbatch --partition=mi250 --ntasks-per-node=16 --export=${COMMON_EXPORTS} --output=${RES_DIR}/results_lat_mi250.txt amd_run_latency.sbatch
 
 export GPU_ARCH=mi210
-export CPU_BIND="--cpu-bind=map_ldom:0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7"
+export MPICMD="mpirun -np 16 --map-by ppr:2:numa --oversubscribe"
 sbatch --partition=mi210 --ntasks-per-node=16 --export=${COMMON_EXPORTS} --output=${RES_DIR}/results_lat_mi210.txt amd_run_latency.sbatch
 
 ####################################################
@@ -23,9 +23,9 @@ sbatch --partition=mi210 --ntasks-per-node=16 --export=${COMMON_EXPORTS} --outpu
 export USE_HIP=1
 
 export GPU_ARCH=mi250
-export CPU_BIND="--cpu-bind=map_ldom:0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7"
+export MPICMD="mpirun -np 16 --map-by ppr:2:numa --oversubscribe"
 sbatch --partition=mi250 --ntasks-per-node=16 --export=${COMMON_EXPORTS} --output=${RES_DIR}/results_lat_mi250-hip.txt amd_run_latency.sbatch
 
 export GPU_ARCH=mi210
-export CPU_BIND="--cpu-bind=map_ldom:0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7"
+export MPICMD="mpirun -np 16 --map-by ppr:2:numa --oversubscribe"
 sbatch --partition=mi210 --ntasks-per-node=16 --export=${COMMON_EXPORTS} --output=${RES_DIR}/results_lat_mi210-hip.txt amd_run_latency.sbatch
